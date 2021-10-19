f9ac354ceceb165dd0a0209b496a17afcfbd9452
kwhippo <kwhippo@users.noreply.github.com>
2021-10-18 21:17:28 -0600

    refactor Vigenere Cipher to use proper class structure

--- cypher/tools/testing.py
+++ cypher/tools/testing.py
@@ -1,12 +1,13 @@
-from cypher.playfaircipher import PlayfairCipher, PlayfairKey
+from cypher.vigenere import VigenereKey, VigenereCipher
+from pprint import pprint
 
-k = PlayfairKey()
+k = VigenereKey()
 k.validate()
 k.print()
 
 pt = 'Hide the gold in the stump'
 
-c = PlayfairCipher(plaintext=pt, key=k)
+c = VigenereCipher(plaintext=pt, key=k)
 c.encrypt()
 print(c.ciphertext)
 c.decrypt()
--- cypher/vigenere.py
+++ cypher/vigenere.py
@@ -1,40 +1,74 @@
-import cypher.caesar
-import cypher.cipher
-import cypher.tools
-import substitution
+from cypher.caesar import create_keys
+from cypher.cipher import Cipher, Key
+from cypher.utilities import get_random_word
+from cypher import substitution
 
 
-def create_vigenere_key(keyword=None):
+def create_key_list(keyword=None, alphabet=Cipher.ALPHABET):
     if keyword is None:
-        keyword = cypher.utilities.get_random_word(source='pokemon')
-    keys = {'keyword': keyword}
-    try:
-        for index, letter in enumerate(keyword.upper()):
-            keys[index] = cypher.caesar.create_caesar_key(letter)
-    except ValueError:
-        print('Invalid keyword')
-    return keys
+        keyword = get_random_word('pokemon')
+    key_list = []
+    for letter in keyword.upper():
+        key_list.append(create_keys(letter, alphabet)['alpha_key'])
+    return key_list
 
 
-def encrypt(text, key=None):
-    encrypted_text = ''
-    if type(key) is dict:
-        keys = key
+def crypt(text, source_alphabet, crypted_alphabet):
+    crypted_text = ''
+    if type(source_alphabet) == list:
+        source_list = True
+        key_length = len(source_alphabet)
+        alphabet = crypted_alphabet
     else:
-        keys = create_vigenere_key(key)
-    for text_index, letter in enumerate(text.upper()):
-        keyword_index = text_index
-        if text_index >= len(keys['keyword']):
-            keyword_index = keyword_index % len(keys['keyword'])
-        encrypted_text += substitution.encrypt(letter, keys[keyword_index]['key'])[0]
-    return encrypted_text, keys
+        source_list = False
+        key_length = len(crypted_alphabet)
+        alphabet = source_alphabet
+    key_index = 0
+    for letter in text.upper():
+        if source_list:
+            crypted_text += substitution.crypt(letter, source_alphabet[key_index], crypted_alphabet)
+        else:
+            crypted_text += substitution.crypt(letter, source_alphabet, crypted_alphabet[key_index])
+        if letter in alphabet:
+            key_index += 1
+            if key_index == key_length:
+                key_index = 0
+    return crypted_text
 
 
-def decrypt(text, keys):
-    decrypted_text = ''
-    for text_index, letter in enumerate(text.upper()):
-        keyword_index = text_index
-        if text_index >= len(keys['keyword']):
-            keyword_index = keyword_index % len(keys['keyword'])
-        decrypted_text += substitution.decrypt(letter, keys[keyword_index]['key'])[0]
-    return decrypted_text
+class VigenereKey(Key):
+    def __init__(self, keyword=None, key_list=None):
+        super(VigenereKey, self).__init__()
+        self.keyword = keyword
+        self.key_list = key_list
+
+    def validate(self, alphabet=Cipher.ALPHABET):
+        if self.keyword is None and self.key_list is None:
+            self.keyword = get_random_word('pokemon')
+        if self.key_list is None:
+            self.key_list = create_key_list(self.keyword, alphabet)
+        if self.keyword is None:
+            keyword = ''
+            for item in self.key_list:
+                keyword += item[0]
+            self.keyword = keyword
+        assert len(self.key_list) == len(self.keyword)
+        for key in self.key_list:
+            key_set = set(key)
+            assert len(key_set) == len(alphabet)
+            for character in alphabet:
+                assert character in key_set
+        super().validate()
+
+
+class VigenereCipher(Cipher):
+    def __init__(self, *args, **kwargs):
+        super(VigenereCipher, self).__init__(*args, **kwargs)
+
+    def encrypt(self):
+        super(VigenereCipher, self).encrypt()
+        self.ciphertext = crypt(self.plaintext, self.alphabet, self.key.key_list)
+
+    def decrypt(self):
+        super(VigenereCipher, self).decrypt()
+        self.plaintext = crypt(self.ciphertext, self.key.key_list, self.alphabet)
