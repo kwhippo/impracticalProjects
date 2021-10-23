37c41b330bcc62782cac64596b6f2d5276495634
kwhippo <kwhippo@users.noreply.github.com>
2021-10-22 23:31:18 -0600

    testing random method

--- cypher/tools/testing.py
+++ cypher/tools/testing.py
@@ -2,15 +2,15 @@ from cypher.vigenere import VigenereKey, VigenereCipher
 from pprint import pprint
 
 k = VigenereKey()
-k.validate()
+k.random()
 k.print()
 
 pt = 'Hide the gold in the stump'
 
-c = VigenereCipher(plaintext=pt, key=k)
-c.encrypt()
-print(c.ciphertext)
-c.decrypt()
-print(c.plaintext)
+# c = VigenereCipher(plaintext=pt, key=k)
+# c.encrypt()
+# print(c.ciphertext)
+# c.decrypt()
+# print(c.plaintext)
 
 
